90108be80066ae7775a0e401f9a7878df483266a
Mohammad Hussein <mhalfakih15@gmail.com>
2021-01-31 13:36:18 +0300

    Add Detail

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link,NavLink } from "react-router-dom";
 
 export default function Navbar({ setQuery }) {
   return (
     <div>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Brand
-          </a>
+          <Link to="/" class="navbar-brand">
+            <img
+              src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_2896,h_2896/http://trekkinghikers.com/wp-content/uploads/2020/10/Trekkinghikers-Logo.png"
+              alt=""
+              srcset=""
+              style={{width:"40px",height:"40px"}}
+            />
+          </Link>
 
           <button
             class="navbar-toggler"
@@ -24,16 +29,12 @@ export default function Navbar({ setQuery }) {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <Link
-                  to="/"
-                  class="nav-link active"
-                  aria-current="page"
-                >
+                <Link to="/" class="nav-link active" aria-current="page">
                   Home
                 </Link>
               </li>
               <li class="nav-item">
-                <Link to="/hiking-trips" class="nav-link" >
+                <Link to="/hiking-trips" class="nav-link">
                   Trips
                 </Link>
               </li>
